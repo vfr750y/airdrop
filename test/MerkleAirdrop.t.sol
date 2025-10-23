671626f9b82e07f4ae3e8eb6907cef8c33541139
vfr750y <th91843@proton.me>
2025-10-24 09:16:23 +1300

    Add inputs and outputs

--- test/MerkleAirdrop.t.sol
+++ test/MerkleAirdrop.t.sol
@@ -2,6 +2,25 @@
 
 pragma solidity ^0.8.24;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {MerkleAirdrop} from "../src/MerkleAirdrop.sol";
+import {BagelToken} from "../src/BagelToken.sol";
 
-contract MerkleAirdropTest is Test {}
+contract MerkleAirdropTest is Test {
+    MerkleAirdrop public airdrop;
+    BagelToken public token;
+
+    bytes32 public ROOT = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
+    address user;
+    uint256 userPrivKey;
+
+    function setUp() public {
+        token = new BagelToken();
+        airdrop = new MerkleAirdrop(ROOT, token);
+        (user, userPrivKey) = makeAddrAndKey("user");
+    }
+
+    function testUsersCanClaim() public {
+        console.log("user address:", user);
+    }
+}
