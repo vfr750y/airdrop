81f8dec1e1d9f7ad9ed9f32312f878b94da5a512
vfr750y <th91843@proton.me>
2025-10-24 09:41:18 +1300

    Add testUsersCanClaim

--- test/MerkleAirdrop.t.sol
+++ test/MerkleAirdrop.t.sol
@@ -11,6 +11,10 @@ contract MerkleAirdropTest is Test {
     BagelToken public token;
 
     bytes32 public ROOT = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
+    uint256 public AMOUNT = (25 * 1e18);
+    bytes32 proofOne = 0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a;
+    bytes32 proofTwo = 0xd1445c931158119b00449ffcac3c947d028c0c359c34a6646d95962b3b55c6ad;
+    bytes32[] public PROOF = [proofOne, proofTwo];
     address user;
     uint256 userPrivKey;
 
@@ -21,6 +25,11 @@ contract MerkleAirdropTest is Test {
     }
 
     function testUsersCanClaim() public {
-        console.log("user address:", user);
+        uint256 startingBalance = token.balanceOf(user);
+        vm.prank(user);
+        airdrop.claim(user, AMOUNT, PROOF);
+        uint256 endingBalance = token.balanceOf(user);
+        console.log("Ending Balalnce:", endingBalance);
+        assertEq(endingBalance - startingBalance, AMOUNT);
     }
 }
