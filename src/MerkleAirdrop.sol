67b1ec708a634a3e0a81291a1ae633516f8c52ff
vfr750y <th91843@proton.me>
2025-10-23 22:27:17 +1300

    add getter functions

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -41,4 +41,8 @@ contract MerkleAirdrop {
         emit Claim(account, amount);
         i_airdropToken.safeTransfer(account, amount);
     }
+
+    function getMerkleRoot() external view returns (bytes32) {
+        return i_merkleRoot;
+    }
 }
